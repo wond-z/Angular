8b13be6ff338f707397e8b172619a2666de43931
wond-z <wond-z@live.com>
2018-05-07 22:53:59 +0800

    angular HERO router

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs/observable/of';
+// import { of } from 'rxjs/observable';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    // Todo: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
+  constructor(private messageService: MessageService) { }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+// import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -9,21 +10,34 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-    heroes: Hero[];
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
 
-    selectedHero: Hero;
+  // heroes = HEROES;
+  heroes: Hero[];
+
+  // selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
 }
+
+// "C:/Users/wond-/Desktop/myapp/my-app/node_modules/rxjs/Rx"' has no exported member 'of'.
